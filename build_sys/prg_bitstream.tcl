b84ea11eeb34a0a7f26425c123125c53bdd64cff
epsilon <ruben.lysens2@gmail.com>
2022-10-26 12:11:51 +0200

    Add make run rule that connects to Arty and programs bitstream

--- build_sys/prg_bitstream.tcl
+++ build_sys/prg_bitstream.tcl
@@ -19,15 +19,11 @@ proc getopt {_argv name {_var ""} {default ""}} {
 }
 
 getopt argv -bitstream bitstream ""
-getopt argv -ip ip ""
 
 puts "bitstream: $bitstream"
-puts "ip: $ip"
 
 open_hw_manager
-connect_hw_server -url $ip:3121 -allow_non_jtag
-current_hw_target [get_hw_targets */xilinx_tcf/Digilent/210319B0C4AEA]
-set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Digilent/210319B0C4AEA]
+connect_hw_server -allow_non_jtag
 open_hw_target
 current_hw_device [get_hw_devices xc7a35t_0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7a35t_0] 0]
@@ -36,3 +32,4 @@ set_property FULL_PROBES.FILE {} [get_hw_devices xc7a35t_0]
 set_property PROGRAM.FILE $bitstream [get_hw_devices xc7a35t_0]
 program_hw_devices [get_hw_devices xc7a35t_0]
 refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0]
+close_hw_manager
