34131e82a10c73452a9dcc876d67ddcd18abf0d1
epsilon <ruben.lysens2@gmail.com>
2023-06-02 10:15:58 +0200

    ym2149 + DAC test SoC. WIP.

--- gw/projects/audio_dac_test/rtl/audio_dac_test.sv
+++ gw/projects/audio_dac_test/rtl/audio_dac_test.sv
@@ -21,13 +21,17 @@ logic [7:0] phase_r;
 logic [8:0] sr_divider_r;
 logic [3:0] gpio0_r;
 logic [31:0] gpio_divider_r;
-logic unused = &{1'b0, sys_rst_n, sw[3], 1'b0};
+logic [1:0] cpt4_r;
+logic signed [15:0] sin_rom_out;
+
+logic unused = &{1'b0, sys_rst_n, 1'b0};
 
 initial begin
     phase_r = 8'h0;
     sr_divider_r = 9'h0;
     gpio_divider_r = 0;
     gpio0_r = 0;
+    cpt4_r = 2'b00;
 end
 
 assign audio_gain = sw[0];
@@ -47,35 +51,50 @@ assign gpio0 = gpio0_r;
   assign sys_rst_n = ext_rst_n;
 `endif //SYNTHESIS/No SYNTHESIS
 
-assign audio_pcm = phase_r > 127 ? (sw[2] ? 32767 : 16383) : (sw[2] ? -32767 : -16383); //16384 : -16384; //half scale amplitude square wave
+sin_rom sin_rom_inst
+   (.clk(sys_clk),
+    .addr_r(phase_r),
+    .dout(sin_rom_out)
+   );
+
+always_comb begin
+  case (sw[3:2])
+    2'b00: audio_pcm = sin_rom_out;
+    2'b01: audio_pcm = {sin_rom_out[15], sin_rom_out[15:1]};
+    2'b10: audio_pcm = {{2{sin_rom_out[15]}}, sin_rom_out[15:2]};
+    2'b11: audio_pcm = {{3{sin_rom_out[15]}}, sin_rom_out[15:3]};
+  endcase
+end
+//assign audio_pcm = phase_r > 127 ? (sw[2] ? 32767 : 16383) : (sw[2] ? -32767 : -16383); //16384 : -16384; //half scale amplitude square wave
 `ifdef VERILATOR
 assign pcm_out = audio_pcm;
 `endif
-reg [1:0] cpt4;
 
 one_bit_dac dac_inst (
   .clk(sys_clk),      // 50MHz clock
-  .clk_en(cpt4==0),   // 12.5MHz clock enable
+  .clk_en(cpt4_r==2'b00),   // 12.5MHz clock enable
   .in(audio_pcm),     // input
   .out(audio_out)     // one bit out modulated at 12.5MHz
   );
 
 always_ff @(posedge sys_clk)
 begin
-    cpt4 <= cpt4 - 1;
-    gpio_divider_r <= gpio_divider_r - 1;
-
-    if (gpio_divider_r == 0) begin
-      gpio_divider_r <= 32'd10000000;
+    cpt4_r <= cpt4_r + 1;
+    
+    if (gpio_divider_r == 32'd10000000) begin
+      gpio_divider_r <= 0;
       gpio0_r <= gpio0_r + 4'd1;
     end
+    else begin
+      gpio_divider_r <= gpio_divider_r + 1;
+    end
 
-    if (sr_divider_r == 0) begin
-        sr_divider_r <= 9'd111;
+    if (sr_divider_r == 9'd444) begin
+        sr_divider_r <= 9'd0;
         phase_r <= phase_r + 8'h01;
     end
     else begin
-        sr_divider_r <= sr_divider_r - 9'h01;
+        sr_divider_r <= sr_divider_r + 9'd01;
     end
 end
 
---
+++ gw/projects/ym2149_dac_test/sim/sim_main.sv
@@ -0,0 +1,96 @@
+//
+//  A simulator top-level wrapping around ibex_soc, instantiating SimJTAG.
+//
+module sim_main #(
+		  parameter int unsigned OPENOCD_PORT = 9999
+		  ) 
+   (
+    input logic      clk_i,
+    input logic      rst_ni,
+    inout wire [7:0] gpio0,
+    inout wire [3:0] gpio1,
+    input wire 	     uart_rx,
+    output wire      uart_tx,
+	// VGA interface
+	output wire  [3:0] vga_r,       
+	output wire  [3:0] vga_g,       
+	output wire  [3:0] vga_b,       
+	output wire        vga_hsync,   
+	output wire        vga_vsync,
+	// SDSPI interface
+  	output wire  sdspi_cs_n, 
+  	output wire  sdspi_sck, 
+  	output wire  sdspi_mosi,
+	input  wire	 sdspi_miso, 
+  	input  wire  sdspi_card_detect,
+	// Audio interface
+	output wire       audio_out,
+	output wire       audio_gain,
+	output wire       audio_shutdown_n,
+	output wire [15:0] pcm_out
+    );
+   
+   // jtag openocd bridge signals
+   logic 	     sim_jtag_tck;
+   logic 	     sim_jtag_tms;
+   logic 	     sim_jtag_tdi;
+   logic 	     sim_jtag_trstn;
+   logic 	     sim_jtag_tdo;
+   logic [31:0]  sim_jtag_exit;
+   
+   SimJTAG  #(
+              .TICK_DELAY (1),
+              .PORT(OPENOCD_PORT)
+	      ) simJTAG_inst(
+			     .clock                ( clk_i                ),
+			     .reset                ( ~rst_ni              ),
+			     .enable               ( 1'b1                 ),
+			     .init_done            ( rst_ni               ),
+			     .jtag_TCK             ( sim_jtag_tck         ),
+			     .jtag_TMS             ( sim_jtag_tms         ),
+			     .jtag_TDI             ( sim_jtag_tdi         ),
+			     .jtag_TRSTn           ( sim_jtag_trstn       ),
+			     .jtag_TDO_data        ( sim_jtag_tdo         ),
+			     .jtag_TDO_driven      ( 1'b1                 ),
+			     .exit                 ( sim_jtag_exit        )
+			     );
+
+   ym2149_dac_test_soc dut (
+		 .ext_clk(clk_i),
+		 .ext_rst_n(rst_ni),
+		 .gpio0(gpio0),
+		 .gpio1(gpio1),
+		 .uart_rx(uart_rx),
+		 .uart_tx(uart_tx),
+		 .tck(sim_jtag_tck),
+		 .trst_n(sim_jtag_trstn),
+		 .tms(sim_jtag_tms),
+		 .tdi(sim_jtag_tdi),
+		 .tdo(sim_jtag_tdo),
+		 .pll_locked_led(),
+		 .init_done_led(),
+		 .init_err_led(),
+		 // VGA interface
+  		 .vga_r(vga_r),       
+  		 .vga_g(vga_g),       
+  		 .vga_b(vga_b),       
+  		 .vga_hsync(vga_hsync),   
+  		 .vga_vsync(vga_vsync),
+		 // SDSPI interface
+  		 .sdspi_cs_n(sdspi_cs_n), 
+  		 .sdspi_sck(sdspi_sck), 
+  		 .sdspi_mosi(sdspi_mosi),
+		 .sdspi_miso(sdspi_miso), 
+  		 .sdspi_card_detect_n(~sdspi_card_detect),
+		 // ym2149 interface
+		.audio_out(audio_out),
+  		.audio_gain(audio_gain),
+  		.audio_shutdown_n(audio_shutdown_n),
+  		.pcm_out(pcm_out)
+		);
+   
+   always_comb begin : jtag_exit_handler
+      if (|sim_jtag_exit)
+        $finish(2); // print stats too
+   end
+endmodule
