b05d756223071f0255eecb39d970c15e64ae046e
epsilon <ruben.lysens2@gmail.com>
2025-06-27 13:11:46 +0200

    Accidental check-in of experimentation code.

--- sw/projects/hello_world/hello.cpp
+++ sw/projects/hello_world/hello.cpp
@@ -8,17 +8,6 @@
 
 #define GPIO_SIM_INDICATOR 0xf0 //If GPIO inputs 7:4 have this value, this is a simulation.
 
-typedef union {
-  uint32_t SETUP;
-  struct {
-    uint32_t BAUD_CLKS : 24; // System clock per baudrate interval
-    uint32_t PFT : 3; // Parity setup
-    uint32_t S : 1; // Number of stop bits - 1
-    uint32_t N : 2; // 8 - number of bits per word
-    uint32_t H : 1; // Disable hardware flow control
-  };
-} setup_t;
-
 //_init is executed by picolibc startup code before main().
 void _init(void) {
   uart_set_baudrate(115200);
@@ -31,14 +20,6 @@ void	_exit (int status) {
 }
 
 int main(void) {
-  setup_t setup;
-
-  setup.SETUP = 0x12345678;
-  printf("Baud = %d\n", setup.BAUD_CLKS);
-
-  setup.PFT = 0;
-  printf("setup =0x%x\n", setup.SETUP);
-
   uint32_t leds = 0xF;
 
   gpio_init();
