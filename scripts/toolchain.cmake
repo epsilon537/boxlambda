005a4fb876cb6f129bfcd56c39c2384bb6f8ad5e
epsilon <ruben.lysens2@gmail.com>
2025-08-16 16:00:15 +0200

    no RTTI

--- scripts/toolchain.cmake
+++ scripts/toolchain.cmake
@@ -26,5 +26,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_ASM_FLAGS "-march=rv32im_zba_zbb_zbs_zicsr -mabi=ilp32 -Wl,--no-warn-rwx-segments")
 set(CMAKE_C_FLAGS "-march=rv32im_zba_zbb_zbs_zicsr -mabi=ilp32 -Wl,--no-warn-rwx-segments")
 # some additional disable options to avoid having to pull in a C++ run-time for exception handling and std c++ library.
-set(CMAKE_CXX_FLAGS "-march=rv32im_zba_zbb_zbs_zicsr -mabi=ilp32 -fno-threadsafe-statics -fno-exceptions -Wl,--no-warn-rwx-segments")
+set(CMAKE_CXX_FLAGS "-march=rv32im_zba_zbb_zbs_zicsr -mabi=ilp32 -fno-threadsafe-statics -fno-rtti -fno-exceptions -Wl,--no-warn-rwx-segments")
 
