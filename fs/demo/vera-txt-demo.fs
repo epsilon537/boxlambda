427c51a5fd9494e564717dd9f8ba19616e73ba9f
epsilon <ruben.lysens2@gmail.com>
2026-08-20 13:43:45 +0200

    vera-txt-demo

--- fs/demo/vera-txt-demo.fs
+++ fs/demo/vera-txt-demo.fs
@@ -1,10 +1,9 @@
-: x-process-fontline-len-err ." Invalid line length in font file error" cr ;
 : x-process-font-dot-star-err ." Font file ./* error" cr ;
-: x-process-font-eof-err ." Unexpected EOF error processing font file" cr ;
 
-0 constant PROCESS-GLYPH_STAT-OK
-1 constant PROCESS-GLYPH_STAT-ERR
-2 constant PROCESS-GLYPH_STAT-EOF
+320 constant XRES
+240 constant YRES
+XRES 8 / constant #COLS
+YRES 8 / constant #ROWS
 
 begin-structure process-glyph-struct
   field: .glyph-base
@@ -38,15 +37,19 @@ create (pgs) process-glyph-struct allot
   [ 0 1 stack-checker ]
   begin
     (pgs) .font-fp @ (pgs) .fontline @ 256 f_gets ( addr len )
-    swap drop ( len )
+    2dup type cr
+    nip ( len )
     dup (pgs) .fontlinelen ! ( len )
-    0= triggers x-process-font-eof-err ( )
-    (pgs) .fontline @ c@ 
-    dup [char] # <> ( char f )
-    over #13 <> ( char f f )
-    rot #10 <> and and ( f )
+    0= if true
+    else
+      (pgs) .fontline @ c@ ( char )
+      dup [char] # <> ( char f )
+      over #13 <> ( char f f )
+      rot #10 <> ( f f f )
+      and and ( f )
+    then
   until
-  (pgs) .fontlinelen @ 8 < ( f )
+  (pgs) .fontlinelen @ 8 >= ( f )
 ;
 
 \ May raise x-process-font-err.
@@ -67,21 +70,21 @@ create (pgs) process-glyph-struct allot
 ;
 
 \ May raise x-process-font-err, x-fr-* and x-pool-* exceptions.
-( tileset filename-addr filename-len -- )
+( tileset filename-addr filename-len --  #glyphs )
 : process-font
-  [ 3 0 stack-checker ]
+  [ 3 1 stack-checker ]
   FA_OPEN_EXISTING FA_READ or f_open ( tileset fil )
-  (pgs) .font-fp ! ( tileset )
-  256 [:
-    (pgs) .fontline ! ( tileset )
-    0 ( tileset tidx )
+  (pgs) .font-fp ! ( tset )
+  256 [: ( tset buf )
+    (pgs) .fontline ! ( tset )
+    0 ( tset tidx )
     begin
+      2dup swap tset-tidx>addr (pgs) .glyph-base ! ( tset tidx )
       process-glyph ( tset tidx f )
       while ( tset tidx )
-        2dup swap tset-tidx>addr (pgs) .glyph-base ! ( tset tidx )
         1+
     repeat
-    2drop ( )
+    nip ( tidx )
   ;] with-temp-allot
 
   (pgs) .font-fp @ f_close
@@ -89,48 +92,80 @@ create (pgs) process-glyph-struct allot
 
 0 0 vec2 variable cursor
 
-: edit-loop
-  [ 0 0 stack-checker ]
-  begin ( )
-    \ Draw the cursor
-    tm mapentry{ [char] _ tidx cursor @ xy WHITE fg BLUE bg }set
-    key ( key )
-    dup #13 = if
-      drop
-      cursor @ vec2.y 1+ ( y )
-      dup #60 = if ( y )
-        drop 0 ( y )
+( -- c )
+: (edit-1)
+  [ 0 1 stack-checker ]
+  \ Draw the cursor
+  tm mapentry{ 0 tidx cursor @ xy YELLOW fg YELLOW bg }set
+  key dup case
+    #27 of endof
+
+    #8 of
+      \ Erase cursor
+      tm mapentry{ 0 tidx cursor @ xy YELLOW fg BLACK bg }set
+      cursor @ vec2.xy ( key x y )
+      swap ( key y x )
+      dup if ( key y x )
+        1- ( key y x-1 )
       then
-      0 swap vec2 cursor !
-    else ( key )
-      $20 -
-      \ draw the character at the cursor position
-      tm mapentry{ ( key ) tidx cursor @ xy YELLOW fg BLUE bg }set
-      cursor @ vec2.xy swap 1+ swap ( x y )
-      over #80 = if ( x y )
-        nip 0 swap ( x y )
-        1+ ( x y )
-        dup #60 = if ( x y )
-          drop 0 ( x y )
-        then
+      swap vec2 cursor ! ( key )
+    endof
+
+    #13 of 
+      \ Erase cursor
+      tm mapentry{ 0 tidx cursor @ xy YELLOW fg BLACK bg }set
+      cursor @ vec2.y 1+ ( key y )
+      dup #ROWS = if ( key y )
+        drop 0 ( key y )
       then
-      vec2 cursor !
+      0 swap vec2 cursor ! ( key )
+    endof
+
+    $20 - ( key tidx )
+    \ draw the character at the cursor position
+    tm mapentry{ ( tidx ) tidx cursor @ xy YELLOW fg BLACK bg }set
+    cursor @ vec2.xy swap 1+ swap ( key x y )
+    over #COLS = if ( key x y )
+      nip 0 swap ( key x y )
+      1+ ( key x y )
+      dup #ROWS = if ( key x y )
+        drop 0 ( key x y )
+      then
     then
-    \ delay
-    100000 0 do loop
+    vec2 cursor ! ( key )
+    dup
+  endcase
+;
+
+( -- )
+: vera-txt-demo
+  ts tset{ 8 width 8 height 1 bpp 256 tiles }set
+  tm tmap{ 64 width 32 height TMAP-TXT16 type }set
+  l0 layer{ ts tset tm tmap }tilemap-mode
+
+  cr
+  ts token process-font
+  ." #glyphs: " . cr
+
+  true l0 layer-enable
+  false l1 layer-enable
+  false sprites-enable
+
+  \ scale to 320x240
+  $40 dup hscale! vscale!
+
+  true display-enable
+
+  ts tset-print
+  tm tmap-print
+  l0 layer-print
+
+  begin 
+    (edit-1) dup .
+    27 = if exit then
+    100000 0 do loop \ delay
   again
 ;
 
-ts tset{ 8 width 8 height 1 bpp 256 tiles }set
-tm tmap{ 128 width 64 height TMAP-TXT16 type }set
-l0 layer{ ts tset tm tmap }tilemap-mode
-
-ts s" altered-chrome.fnt" process-font
-
-true l0 layer-enable
-false l1 layer-enable
-false sprites-enable
-true display-enable
-
-edit-loop
+vera-txt-demo night-in-tokyo.fnt
 
